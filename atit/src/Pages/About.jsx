6081ab12219066d510b7a9d7422e67ee849baf53
devbharath21k <devbharath21k@gmail.com>
2024-01-04 13:59:57 +0530

    new

--- atit/src/Pages/About.jsx
+++ atit/src/Pages/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Company from '../Components/Company'
+import Footer from '../Components/Footer'
 
 function About() {
   return (
@@ -80,73 +82,79 @@ function About() {
         </div>
         {/* atit-HowItWorks */}
         <div>
-        <div id="atit-HowItWorks" class="atit-HowItWorks">
-        <div class="cube"></div>
-        <div class="cube"></div>
-        <div class="cube"></div>
-        <div class="cube"></div>
-        <div class="cube"></div>
-        <div class="cube"></div>
-        <div class="container position-relative z-3">
-            <div class="row ">
+          <div id="atit-HowItWorks" class="atit-HowItWorks">
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="cube"></div>
+            <div class="container position-relative z-3">
+              <div class="row ">
                 <div class="col-lg-5 d-flex align-items-center justify-content-center">
-                    <div class="atit-HowItWorks-header">
-                        <h3 class="text-center" data-aos="fade-right" data-aos-duration="500">How It Works</h3>
-                        <p class="text-center" data-aos="fade-right" data-aos-duration="700">
-                            We are dedicated to helping you achieve your goals and reach your highest potential
-                        </p>
-                        <img src="./assets/imagesandvectors/bg/contact/steps.png" class="col-md-9 rounded img-fluid"
-                            alt="about img"/>
-                    </div>
+                  <div class="atit-HowItWorks-header">
+                    <h3 class="text-center" data-aos="fade-right" data-aos-duration="500">How It Works</h3>
+                    <p class="text-center" data-aos="fade-right" data-aos-duration="700">
+                      We are dedicated to helping you achieve your goals and reach your highest potential
+                    </p>
+                    <img src="./assets/imagesandvectors/bg/contact/steps.png" class="col-md-9 rounded img-fluid"
+                      alt="about img" />
+                  </div>
                 </div>
                 <div class="col-lg-7">
-                    <div class="row align-items-center ">
-                        <div class="col-lg-6 HowItWorks-margin">
-                            <div class="HowItWorks-content">
-                                <div class="HowItWorks-item">
-                                    <div class="icon">
-                                        <i class="ri-team-fill"></i>
-                                    </div>
-                                    <p class="head" data-aos="fade-up" data-aos-duration="600">1. Learn</p>
-                                    <p data-aos="fade-up" data-aos-duration="700">
-                                        Enhance your skills and harness your potential with our cutting-edge software
-                                        training programs.
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="HowItWorks-content">
-                                <div class="HowItWorks-item">
-                                    <div class="icon">
-                                        <i class="ri-graduation-cap-line"></i>
-                                    </div>
-                                    <p class="head" data-aos="fade-up" data-aos-duration="600">2. Get Certificate</p>
-                                    <p data-aos="fade-up" data-aos-duration="800">
-                                        Validate your expertise and elevate your career with our industry-recognized
-                                        certifications.
-                                    </p>
-                                </div>
-                            </div>
+                  <div class="row align-items-center ">
+                    <div class="col-lg-6 HowItWorks-margin">
+                      <div class="HowItWorks-content">
+                        <div class="HowItWorks-item">
+                          <div class="icon">
+                            <i class="ri-team-fill"></i>
+                          </div>
+                          <p class="head" data-aos="fade-up" data-aos-duration="600">1. Learn</p>
+                          <p data-aos="fade-up" data-aos-duration="700">
+                            Enhance your skills and harness your potential with our cutting-edge software
+                            training programs.
+                          </p>
                         </div>
-                        <div class="col-lg-6">
-                            <div class="HowItWorks-content">
-                                <div class="HowItWorks-item">
-                                    <div class="icon">
-                                        <i class="ri-computer-line"></i>
-                                    </div>
-                                    <p class="head" data-aos="fade-up" data-aos-duration="600">3. Work</p>
-                                    <p data-aos="fade-up" data-aos-duration="700">
-                                        Step into a world of opportunities with a guaranteed job offer upon completing
-                                        our comprehensive
-                                        training.
-                                    </p>
-                                </div>
-                            </div>
+                      </div>
+                      <div class="HowItWorks-content">
+                        <div class="HowItWorks-item">
+                          <div class="icon">
+                            <i class="ri-graduation-cap-line"></i>
+                          </div>
+                          <p class="head" data-aos="fade-up" data-aos-duration="600">2. Get Certificate</p>
+                          <p data-aos="fade-up" data-aos-duration="800">
+                            Validate your expertise and elevate your career with our industry-recognized
+                            certifications.
+                          </p>
                         </div>
+                      </div>
                     </div>
+                    <div class="col-lg-6">
+                      <div class="HowItWorks-content">
+                        <div class="HowItWorks-item">
+                          <div class="icon">
+                            <i class="ri-computer-line"></i>
+                          </div>
+                          <p class="head" data-aos="fade-up" data-aos-duration="600">3. Work</p>
+                          <p data-aos="fade-up" data-aos-duration="700">
+                            Step into a world of opportunities with a guaranteed job offer upon completing
+                            our comprehensive
+                            training.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
+        {/* company */}
+        <Company/>
+        <div>
+          {/* footer */}
+          <Footer/>
         </div>
       </div>
     </div>
