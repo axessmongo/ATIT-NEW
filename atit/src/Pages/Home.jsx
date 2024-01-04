77a3aab32be499557b7ec7062a19b0aa59e57909
nivethaaxess <nivetha.rajendran@axesstechnology.in>
2024-01-04 18:12:22 +0530

    jay

--- atit/src/Pages/Home.jsx
+++ atit/src/Pages/Home.jsx
@@ -2,19 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
 import { Link } from 'react-router-dom'
-import ContactForm from '../Components/ContactForm'
-import Rocket from '../Components/Rocket'
-import Company from '../Components/Company'
-import LeftBar from '../Components/LeftBar'
 function Home() {
-  useEffect(() => {
-    var mbnew = document.querySelector('.card-img-overlay').offsetHeight + 100;
-    document.querySelector('.hero-banner').style.marginBottom = mbnew + 'px';
-  }, [])
-
-  setTimeout(() => {
-    document.querySelector('[data-bs-target="#contact"]').click();
-  }, 500);
   return (
     <div>
       <Navbar />
@@ -451,7 +439,7 @@ function Home() {
             class="btn primary-btn bg-primary text-white mb-5 mx-auto"
           >About PAP
           </Link>
-
+          
         </div>
       </div>
       {/* DevelopmentProgram */}
@@ -654,16 +642,37 @@ function Home() {
         </div>
       </section>
       {/* Our Trained Students are Hired By */}
-
-      <Company />
+      <div class="container company-logos mb-5 overflow-hidden">
+        <p
+          class="text-center text-capitalize justify-content-center mt-4 display-6 linear-gradient-text fw-semibold"
+          data-aos="fade-up"
+          data-aos-duration="7000"
+        >
+          Our Trained Students are Hired By
+        </p>
+        <div class="row">
+          <div class="col-md-2" data-aos="fade-right" data-aos-duration="1000">
+            <img src="./assets/img/1.png" class="img-fluid" alt="company " />
+          </div>
+          <div class="col-md-2" data-aos="fade-right" data-aos-duration="1000">
+            <img src="./assets/img/2.png" class="img-fluid" alt="company " />
+          </div>
+          <div class="col-md-2" data-aos="fade-right" data-aos-duration="1000">
+            <img src="./assets/img/3.png" class="img-fluid" alt="company " />
+          </div>
+          <div class="col-md-2" data-aos="fade-left" data-aos-duration="1000">
+            <img src="./assets/img/4.png" class="img-fluid" alt="company " />
+          </div>
+          <div class="col-md-2" data-aos="fade-left" data-aos-duration="1000">
+            <img src="./assets/img/5.png" class="img-fluid" alt="company " />
+          </div>
+          <div class="col-md-2" data-aos="fade-left" data-aos-duration="1000">
+            <img src="./assets/img/6.png" class="img-fluid" alt="company " />
+          </div>
+        </div>
+      </div>
       {/* footer */}
       <Footer />
-      {/* {contact Form} */}
-      <ContactForm />
-      {/* {Rocket} */}
-      <Rocket />
-      {/* {LeftBar} */}
-      <LeftBar/>
     </div>
   )
 }
