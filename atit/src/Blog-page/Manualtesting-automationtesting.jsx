ceba00abed84604f7e3fa8143d3f3767d63032ce
devbharath21k <devbharath21k@gmail.com>
2024-01-10 16:25:09 +0530

    'new'

---
+++ atit/src/Blog-page/Manualtesting-automationtesting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Manualtestingautomationtesting() {
+  return (
+    <div>Manualtesting-automationtesting</div>
+  )
+}
+
+export default Manualtestingautomationtesting
